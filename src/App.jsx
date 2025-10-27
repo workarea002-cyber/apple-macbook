2371727330b46a702f8477045df6df85514c7fbb
workarea002-cyber <workarea002@gmail.com>
2025-10-27 13:28:30 +0530

    productviewer section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
+import ProductViewer from "./components/ProductViewer";
+
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </>
   );
 };
