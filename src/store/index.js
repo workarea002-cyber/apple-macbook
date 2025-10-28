f76f31d8d9457e86386aab17c483133116062ecf
workarea002-cyber <workarea002@gmail.com>
2025-10-28 09:38:55 +0530

    implement features section

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,9 @@ const useMacbookStore = create((set) => ({
   scale: 0.08,
   setScale: (scale) => set({ scale }),
 
+  texture: "/videos/feature-1.mp4",
+  setTexture: (texture) => set({ texture }),
+
   reset: () => set({ color: "#2e2c2e", scale: 0.08 }),
 }));
 
